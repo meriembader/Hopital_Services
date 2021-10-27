bed9e303aba38e466504556f2c598ec23d84140e
alibekir23 <49046280+alibekir23@users.noreply.github.com>
2021-10-27 20:52:41 +0100

    commande update

---
+++ commandes/src/main/java/com/microservices/commandes/CommandesApplication.java
@@ -0,0 +1,14 @@
+package com.microservices.commandes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CommandesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommandesApplication.class, args);
+	}
+
+}
