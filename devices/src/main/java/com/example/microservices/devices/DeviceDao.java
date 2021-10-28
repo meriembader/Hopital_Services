e540a8679d4223df71792442b5755ef90304217f
alibekir23 <49046280+alibekir23@users.noreply.github.com>
2021-10-28 02:28:19 +0100

    api-gateway done

--- devices/src/main/java/com/example/microservices/devices/DeviceDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.microservices.devices;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeviceDao extends MongoRepository<Device, Integer>{
-
-}
-
